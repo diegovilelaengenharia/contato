19872132d7931f48a91522b7571c1c4a3c41aba7
Diego Vilela <diegonunesvilela@gmail.com>
2025-12-21 09:19:03 -0300

    chore: enable and configure db.php connection

---
+++ area-cliente/db.php
@@ -0,0 +1,22 @@
+<?php
+// Configurações do Banco de Dados
+$host = 'localhost';
+$db   = 'u884436813_cliente';
+$user = 'u884436813_vilela';
+$pass = 'Diego@159753';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+
+try {
+    $pdo = new PDO($dsn, $user, $pass, $options);
+} catch (\PDOException $e) {
+    // Em produção, a mensagem será capturada pelo index.php
+    throw new \PDOException($e->getMessage(), (int)$e->getCode());
+}
+?>
