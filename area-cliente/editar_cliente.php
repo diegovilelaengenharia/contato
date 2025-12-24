0a28ca5a2504e01f08e24691ede6f69fcfcd00e2
Diego Vilela <diegonunesvilela@gmail.com>
2025-12-24 07:09:17 -0300

    Fix 500 Error: Correct require path and enable error reporting

--- area-cliente/editar_cliente.php
+++ area-cliente/editar_cliente.php
@@ -1,12 +1,16 @@
 <?php
 // editar_cliente.php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 session_start();
 if (!isset($_SESSION['admin_logged']) || $_SESSION['admin_logged'] !== true) {
     header('Location: ../login.php');
     exit;
 }
 
-require '../db.php';
+require 'db.php';
 
 $cliente_id = $_GET['id'] ?? null;
 if (!$cliente_id) {
