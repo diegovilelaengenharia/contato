ad24a35d3e91ef016fab1b29d8747d478192832b
Diego Vilela <diegonunesvilela@gmail.com>
2026-01-03 14:45:33 -0300

    chore: enable Vilela Identity in dashboard

--- area-cliente/dashboard.php
+++ area-cliente/dashboard.php
@@ -16,8 +16,8 @@ if (!isset($_SESSION['cliente_id'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Vilela Engenharia | Área do Cliente</title>
     <!-- Updated Assets from 'client-app' build -->
-    <script type="module" crossorigin src="./app/assets/index-DxGpZJ8w.js"></script>
-    <link rel="stylesheet" crossorigin href="./app/assets/index-CeosvJV6.css">
+    <script type="module" crossorigin src="./app/assets/index-D2e2zR_M.js"></script>
+    <link rel="stylesheet" crossorigin href="./app/assets/index-D-oW3_17.css">
   </head>
   <body>
     <div id="root"></div>
