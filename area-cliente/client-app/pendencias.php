6995403ab5df0946d137f9bc5d398fd0d1326342
Diego Vilela <diegonunesvilela@gmail.com>
2026-01-08 19:37:13 -0300

    Debug: Enable errors again

--- area-cliente/client-app/pendencias.php
+++ area-cliente/client-app/pendencias.php
@@ -1,8 +1,8 @@
 <?php
-// DISABLE DEBUGGING FOR PRODUCTION
-ini_set('display_errors', 0);
-ini_set('display_startup_errors', 0);
-error_reporting(0);
+// ENABLE DEBUGGING (AGAIN)
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 session_set_cookie_params(0, '/');
 session_name('CLIENTE_SESSID');
